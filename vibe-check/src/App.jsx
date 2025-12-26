8908b714d6c6c86542835a8c086846f67261fa9e
Nisinii <nisiniweerathunga1814@gmail.com>
2025-12-26 22:54:12 +0100

    Updated App.jsx to use the newly created components

--- vibe-check/src/App.jsx
+++ vibe-check/src/App.jsx
@@ -1,19 +1,10 @@
-import { useGeolocation } from './hooks/useGeolocation';
-import MapDisplay from './components/MapDisplay';
+import React from 'react';
+import Home from './pages/Home';
 
 function App() {
-  const { coords, loading, error } = useGeolocation();
-
-  if (loading) return <div>Locating you...</div>;
-  if (error) return <div>Error: {error}. Please enable location.</div>;
-
   return (
-    <div className="app-container">
-      <h1>Mood Mapper</h1>
-      {/* Default to NYC if coords fail, otherwise use user coords */}
-      <MapDisplay center={coords || { lat: 40.7128, lng: -74.0060 }} />
-    </div>
+    <Home />
   );
 }
 
-export default App
+export default App;
