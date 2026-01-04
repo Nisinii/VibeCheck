bb142542227b6ae3ed49c81867366974d7aad1a1
Nisinii <nisiniweerathunga1814@gmail.com>
2026-01-04 20:12:20 +0100

    Updated the app.jsx with the new pages links

--- vibe-check/src/App.jsx
+++ vibe-check/src/App.jsx
@@ -6,6 +6,8 @@ import Home from './pages/Home';
 // Make sure this matches your actual filename! You mentioned 'getDirections.jsx' but imported PlaceDetails
 import PlaceDetails from './pages/PlaceDetails'; 
 import DeveloperPage from './pages/DeveloperPage';
+import AboutPage from './pages/AboutPage';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
@@ -17,6 +19,8 @@ const App = () => {
         {/* The :id allows us to grab the place ID from the URL */}
         <Route path="/place/:id" element={<PlaceDetails />} />
         <Route path="/developer" element={<DeveloperPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
